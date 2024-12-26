1395214b6c4d181120e7cecba307a1ff06155ac5
Jack Dickinson <jackdickinson090@hotmail.com>
2024-12-26 21:15:32 +0000

    chef claude app update

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <img src="src/assets/chef-claude-icon.png" alt="" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+};
+
+export default Header;
